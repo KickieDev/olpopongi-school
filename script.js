97f45b188c66f169528fe9bdb4f413d4612acaec
KickieDev <wesleyngetich798@gmail.com>
2026-06-28 10:07:36 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// My first JavaScript file
+console.log("Hello from Kickie Developer!");
+
+const myName = "Kickie Developer";
+const daysLearning = 15;
+let isExcited = true;
+
+console.log(myName);
+console.log(daysLearning);
+console.log(isExcited);
+
+console.log(typeof myName);
+console.log(typeof daysLearning);
+console.log(typeof isExcited);
